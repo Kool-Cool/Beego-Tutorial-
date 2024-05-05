42d68189eed4588ec511a1c005d26edad5b5779f
Kool-Cool <omgrabpoint@gmail.com>
2024-05-05 11:28:06 +0530

    Hello World

--- myapp/controllers/default.go
+++ myapp/controllers/default.go
@@ -13,3 +13,8 @@ func (c *MainController) Get() {
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
 }
+
+
+func (hello *MainController) SayHello(){
+	hello.TplName = "hello.html"
+}
